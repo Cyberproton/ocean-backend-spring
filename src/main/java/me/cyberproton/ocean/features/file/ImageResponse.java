a0ab1bcae9659cf33e1a1ff80b50e9af76d98522
Cyberproton <cyberprotonatmailbox@gmail.com>
2024-04-03 22:37:29 +0700

    refactor: pass add image mapper as param

---
+++ src/main/java/me/cyberproton/ocean/features/file/ImageResponse.java
@@ -0,0 +1,15 @@
+package me.cyberproton.ocean.features.file;
+
+import lombok.Builder;
+import me.cyberproton.ocean.util.ImageUrlMapper;
+
+@Builder
+public record ImageResponse(
+        String url
+) {
+    public static ImageResponse fromEntity(FileEntity imageFile, ImageUrlMapper imageUrlMapper) {
+        return ImageResponse.builder()
+                .url(imageUrlMapper.mapFileToUrl(imageFile))
+                .build();
+    }
+}
